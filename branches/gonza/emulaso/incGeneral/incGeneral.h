7c64b725c01a7079b7959956c2df9ab35e07eb67
gonza.w3 <gonza.w3@376daa97-823a-0410-a03a-176a98283959>
2007-10-05 05:14:19 +0000

    Initial import.

---
+++ branches/gonza/emulaso/incGeneral/incGeneral.h
@@ -0,0 +1,60 @@
+/*
+  *<<---------------- BATMAN & ROUND ROBIN ----------------------------->>
+ * Archivo: incGeneral.h
+ * 
+ * Historia
+ *		
+ *
+ ------------------------------------------------------------------ 
+ */
+
+#ifndef INCGENERAL_H_
+#define INCGENERAL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE	1
+#define FALSE	0
+
+#define 	OK		0
+#define 	ERROR	-1
+#define _SIN_REPET_		1
+
+#define CONN_BACKLOG		10
+
+#define LEN_IP	16
+
+#define LEN_PATH_USUARIOS	100
+
+#define LEN_MAX_LINEA_ARCH_USUARIOS		100
+
+#define LEN_USERNAME	20
+#define LEN_PASSWORD	20
+#define LEN_NOMBRE_MSHELL	20
+
+/* Registro de los procesos */
+#define _ID_MSHELL_	0
+#define _ID_ADS_	1
+#define _ID_ACR_	2
+#define _ID_ADP_	3
+#define _ID_PPCB_	4
+
+#define _MSHELL_	"MSHELL"
+#define _ADS_		"ADS"
+/* ----------------------- */
+
+#define bzero(donde, cuanto) memset(donde, '\0', cuanto); /* Como que no es ansi? */
+
+/* Prototipos globales */
+int getCadT(FILE *flujo, int fin, char *cad);
+
+int ContarCharEnString( const char *szString, char cCharAContar );
+
+char* AplicarXorEnString(char *szStringOrigen, int clave);
+
+#define _LARGO_CMD_	100
+
+#endif /*INCGENERAL_H_*/
+/*--------------------------< FIN ARCHIVO >-----------------------------------------------*/
